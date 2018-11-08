50adaea799899ecf50f22ccf90c4eb97bf8c1bd5
Jean <jeannguyen.ca@gmail.com>
2018-11-08 01:21:35 -0800

    Fix webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const entryFile = path.resolve(__dirname, 'client/src', 'index.js');
+const entryFile = path.resolve(__dirname, 'client', 'src', 'index.js');
 const outputDir = path.resolve(__dirname, 'public');
 module.exports = {
   entry: [entryFile],
