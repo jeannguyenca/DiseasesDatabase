a34f1c986c9da96800c1b92cea9ff1969c558e60
Jean <jeannguyen.ca@gmail.com>
2018-11-08 21:50:06 -0800

    Config server.js

--- backend/server.js
+++ backend/server.js
@@ -170,5 +170,5 @@ app.use('/api', router);
 
 var server = app.listen(process.env.PORT || 5000, function() {
   var port = server.address().port;
-  console.log("Express is working on port " + port);
+  console.log("Express is working on port " + port + "and looking for folder " + __dirname);
 });
